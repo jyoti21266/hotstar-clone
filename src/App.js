88f5202f8f0cf9f91d6da012ba83341ecbdd58ba
jyoti21266 <jyoti21266@gmail.com>
2023-02-01 10:50:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path = "/" element = {<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Firebase.js
@@ -0,0 +1,21 @@
+import {initializeApp} from "./firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9VsdIXkeJUbpDqdc6UBtmm8It0TVj45Y",
+    authDomain: "hotstar-clone-30f95.firebaseapp.com",
+    projectId: "hotstar-clone-30f95",
+    storageBucket: "hotstar-clone-30f95.appspot.com",
+    messagingSenderId: "298244209371",
+    appId: "1:298244209371:web:68c2b79541cae42ef82afe"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = db.auth();
+  const provider = new db.auth.GoogleAuthProvider();
+  const storage = db.storage();
+
+  //const { auth,provider,storage};
+  export {db, auth, provider};
+
+  
